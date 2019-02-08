0ad005761395b443190f0008ba25f9cc4d29c8b5
AlexYeates <hi@alexyeates.com>
2019-02-08 17:34:10 +0000

    added message for IE users

--- src/js/modules/internet-explorer.js
+++ src/js/modules/internet-explorer.js
@@ -3,5 +3,6 @@ const isIE =
   !!navigator.userAgent.match(/MSIE/g);
 
 if (isIE) {
-  document.body.innerHTML = 'Please download a real browser';
+  document.body.innerHTML =
+    '<p class="chromeframe"> You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> or <a href="https://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a>to improve your experience.</p>';
 }
