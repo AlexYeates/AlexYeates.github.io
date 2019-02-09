d691739fcf2f818f15847d15b83011ac05d1f9c3
AlexYeates <AlexYeates@hotmail.com>
2019-02-09 00:20:56 +0000

    Seperated out analytics into own module

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,3 @@
 import '../scss/main.scss';
+import './modules/analytics.js';
 import './modules/internet-explorer.js';
-
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-  dataLayer.push(arguments);
-}
-
-gtag('js', new Date());
-gtag('config', 'UA-120742325-1');
-
-const trackOutboundLink = url => {
-  if (window.gtag && gtag.loaded) {
-    gtag('event', 'click', {
-      event_category: 'outbound',
-      event_label: url,
-      transport_type: 'beacon',
-      event_callback: () => {
-        window.open(url, '_blank');
-      }
-    });
-  } else {
-    window.open(url, '_blank');
-  }
-};
-
-const links = Array.from(document.querySelectorAll('a'));
-links.map(e =>
-  e.addEventListener('click', event => {
-    event.preventDefault();
-    trackOutboundLink(e.href);
-  })
-);
