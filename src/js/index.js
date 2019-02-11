e9b805505047f2c347ac6b4887b0c88764d040fd
AlexYeates <hi@alexyeates.com>
2019-02-11 11:41:45 +0000

    Added Polyfill for Array.from

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,4 @@
 import '../scss/main.scss';
+import '../js/modules/polyfill.js';
+import '../js/modules/internet-explorer.js';
 import './modules/analytics.js';
-
-const isIE =
-  !!navigator.userAgent.match(/Trident/g) ||
-  !!navigator.userAgent.match(/MSIE/g);
-
-if (isIE) {
-  document.body.innerHTML =
-    '<p style="text-align: center;"> You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a>!</p>';
-}
---
+++ src/js/modules/internet-explorer.js
@@ -0,0 +1,8 @@
+const isIE =
+  !!navigator.userAgent.match(/Trident/g) ||
+  !!navigator.userAgent.match(/MSIE/g);
+
+if (isIE) {
+  document.body.innerHTML =
+    '<p style="text-align: center;"> You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a>!</p>';
+}
