34ea41f252fb1c0aacfa699d840cacffa47e489f
AlexYeates <hi@alexyeates.com>
2019-02-11 11:35:54 +0000

    Moved IE exception to top-level

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import '../scss/main.scss';
 import './modules/analytics.js';
-import './modules/internet-explorer.js';
+
+const isIE =
+  !!navigator.userAgent.match(/Trident/g) ||
+  !!navigator.userAgent.match(/MSIE/g);
+
+if (isIE) {
+  document.body.innerHTML =
+    '<p style="text-align: center;"> You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a>!</p>';
+}
