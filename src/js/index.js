1b24f3a90ccde21616eb128f444c74810a52ef47
AlexYeates <hi@alexyeates.com>
2019-11-04 09:11:52 +0000

    ..

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import '../scss/main.scss';
 import './components/avatar/avatar.js';
-import './components/internet-explorer/internet-explorer.js';
 import './components/analytics/analytics.js';
 import './components/background/background.js';
